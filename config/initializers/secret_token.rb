6fa850881a7e07b8c01356526dee3cfbda750490
sanek83 <4shureg@gmail.com>
2012-10-01 00:54:19 +0200

    rails skeletton-initial push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShuregDe::Application.config.secret_token = '48ad4aa543e8f14003dc2b8f32281ca85dd9f4fac72c8d9389e2cd7723022d178ee2a77bef5e9274a6fffd5a944d0f33236b83f1902eddacd181815aee65286c'
